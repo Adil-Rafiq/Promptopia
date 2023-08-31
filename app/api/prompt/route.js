2a05843302038517f8f1e6e68fc948188a6a7888
Adil-Rafiq <madilkhan13569@gmail.com>
2023-08-31 09:30:52 +0500

    feed issue

--- app/api/prompt/route.js
+++ app/api/prompt/route.js
@@ -2,10 +2,13 @@ import { connectToDB } from "@utils/database";
 import Prompt from "@models/prompt";
 
 export const GET = async (req, res) => {
+  console.log("Sending all posts");
   try {
     await connectToDB();
 
+    console.log("Connected to DB");
     const prompts = await Prompt.find({}).populate("creator");
+    console.log("Got prompts:", prompts);
     return new Response(JSON.stringify(prompts), { status: 201 });
   } catch (error) {
     return new Response("Failed to fetch all prompts", { status: 201 });
